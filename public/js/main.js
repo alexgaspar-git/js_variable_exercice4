604b034d62a2e288017c723129b81188e5c5f40d
Alex <agasparr98@gmail.com>
2020-12-28 15:18:36 +0100

    fini exo

---
+++ public/js/main.js
@@ -0,0 +1,21 @@
+// let nom = "gaspar"
+// let prenom = "alex"
+// let ville = prompt("Ville?")
+// alert(ville)
+// console.log(nom + " " + prenom)
+
+// console.log(`${prenom} ${nom}`)
+
+let nom = "gaspar"
+let prenom = "alex"
+let villeDep = "Bruxelles"
+let villeDes = "Paris"
+
+console.log(`Le voyage de ${villeDep} vers ${villeDes} a été enregistré au nom de ${prenom} ${nom}`)
+
+let nomP = prompt("Nom?")
+let prenomP = prompt("Prenom?")
+let depP = prompt("Ville de départ?")
+let desP = prompt("Ville de déstination?")
+
+console.log(`Le voyage de ${depP} vers ${desP} a été enregistré au nom de ${nomP} ${prenomP}`)
